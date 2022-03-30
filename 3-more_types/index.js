04581deae0d326f10e2dbd3060f95ba09f5eef6d
Daniel Figueiredo <dt.figueiredo90@gmail.com>
2022-03-29 23:58:12 -0300

    functions

--- 3-more_types/index.js
+++ 3-more_types/index.js
@@ -6,3 +6,11 @@ console.log(arrayOfNumbers);
 console.log(arrayOfNumbers2);
 const arrayOfStrings = ["Ikki", "Pippin", "Tony"];
 console.log(arrayOfStrings);
+//FUNCTIONS
+const myFunction = (name, birthYear) => {
+    let output = `Olá ${name}, você nasceu em ${birthYear}.`;
+    return output;
+};
+console.log(myFunction("Daniel", 1990));
+const greeting = (name) => `Olá ${name}`;
+console.log(greeting("Ikkizinho"));
--- 3-more_types/index.ts
+++ 3-more_types/index.ts
@@ -5,3 +5,13 @@ console.log(arrayOfNumbers);
 console.log(arrayOfNumbers2);
 const arrayOfStrings: string[] = ["Ikki", "Pippin", "Tony"];
 console.log(arrayOfStrings);
+
+//FUNCTIONS
+const myFunction = (name: string, birthYear: number): string => {
+  let output: string = `Olá ${name}, você nasceu em ${birthYear}.`;
+  return output;
+};
+console.log(myFunction("Daniel", 1990));
+
+const greeting = (name: string): string => `Olá ${name}`;
+console.log(greeting("Ikkizinho"));
